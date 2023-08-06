db3bd3b4f99fd570cf958d27174c3e83ade36648
Dhyuti Tewani <55708942+Dhyuti@users.noreply.github.com>
2023-08-06 22:10:02 +0530

    find one and update

--- Server/routes.js
+++ Server/routes.js
@@ -100,7 +100,7 @@ app.put('/plants/:id', (req, res) => {
   const plantId = req.params.id;
   const updateData = req.body;
 
-  Plant.findByIdAndUpdate({plantID: plantId}, updateData)
+  Plant.findOneAndUpdate({plantID: plantId}, updateData)
     .then((updatedPlant) => {
       if (!updatedPlant) {
         return res.status(404).json({ error: 'Plant not found' });
